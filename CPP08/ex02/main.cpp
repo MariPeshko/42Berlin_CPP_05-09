c8b9f6a5de12374c1a50eff844de1ca696031bde
MariPeshko <marunapeshko@gmail.com>
2025-08-21 21:07:51 +0200

    Cpp08 is started

---
+++ CPP08/ex02/main.cpp
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mpeshko <mpeshko@student.42berlin.de>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/21 19:24:18 by mpeshko           #+#    #+#             */
+/*   Updated: 2025/08/21 19:43:34 by mpeshko          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "MutantStack.hpp"
+#include <iostream>
+
+/* int main()
+{
+MutantStack<int> mstack;
+mstack.push(5);
+mstack.push(17);
+std::cout << mstack.top() << std::endl;
+mstack.pop();
+std::cout << mstack.size() << std::endl;
+mstack.push(3);
+mstack.push(5);
+mstack.push(737);
+//[...]
+mstack.push(0);
+MutantStack<int>::iterator it = mstack.begin();
+MutantStack<int>::iterator ite = mstack.end();
+++it;
+--it;
+while (it != ite)
+{
+std::cout << *it << std::endl;
+++it;
+}
+std::stack<int> s(mstack);
+return 0;
+} */
---
+++ CPP08/subject.md
@@ -0,0 +1,14 @@
+Module-specific rules
+
+You will notice that, in this module, the exercises can be solved WITHOUT the standard
+Containers and WITHOUT the standard Algorithms.
+
+However, using them is precisely the goal of this Module.
+You must use the STL — especially the Containers (vector/list/map/and so forth)
+and the Algorithms (defined in header <algorithm>) — whenever they are appropriate.
+Moreover, you should use them as much as you can.
+Thus, do your best to apply them wherever it’s appropriate.
+
+You can define your templates in the header files as usual. Or, if you want to, you
+can write your template declarations in the header files and write their implementations
+in .tpp files. In any case, the header files are mandatory while the .tpp files are optional.
