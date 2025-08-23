78e701f0361eb8c645ba078d2612c34f617f55c4
MariPeshko <marunapeshko@gmail.com>
2025-08-23 16:00:06 +0200

    cpp08 ex00 done

--- CPP08/ex00/easyfind.hpp
+++ CPP08/ex00/easyfind.hpp
@@ -6,7 +6,7 @@
 /*   By: mpeshko <mpeshko@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/21 19:25:02 by mpeshko           #+#    #+#             */
-/*   Updated: 2025/08/22 21:03:54 by mpeshko          ###   ########.fr       */
+/*   Updated: 2025/08/23 15:56:19 by mpeshko          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,51 +23,13 @@
  * Assuming T is a container of integers, this function has to find 
  * the first occurrence of the second parameter in the first parameter.
  * 
- * Benefits of returning iterator: STL consistency: Follows standard 
- * library conventions.
+ * If value not found, returns container.end() (STL convention)
  */
-/* template < typename T>
-int easyfind(T & container, int target ) {
-    for (typename T::iterator it = container.begin(); it != container.end(); ++it)
-        std::cout << *it << " ";
-    std::cout << std::endl;
-    return container[0];
-} */
-
-/* template<typename T>
-typename T::iterator easyfind(T& container, int value) {
-    typename T::iterator it = std::find(container.begin(), container.end(), value);
-    if (it == container.end()) {
-        throw std::runtime_error("Value not found");
-    }
-    return it;
-} */
-
-/* template<typename T>
-typename T::iterator easyfind(T& container, int value) {
-    return std::find(container.begin(), container.end(), value);
-    // Returns container.end() if not found (STL convention)
-} */
-
 template<typename T>
-void easyfind(T& container, int value) {
-	if (std::find(container.begin(), container.end(), value) == container.end())
-		std::cout << "container does not contain " << value << '\n';
-	else
-		std::cout << "container contains " << value << '\n';
-    //return std::find(container.begin(), container.end(), value);
-    // Returns container.end() if not found (STL convention)
-}
+typename T::iterator	easyfind(T& container, int value) {
+	
+	return std::find(container.begin(), container.end(), value);
 
-/* void example_contains()
-{
-    const auto haystack = {1, 2, 3, 4};
- 
-    for (const int needle : {3, 5})
-        if (std::find(haystack.begin(), haystack.end(), needle) == haystack.end())
-            std::cout << "haystack does not contain " << needle << '\n';
-        else
-            std::cout << "haystack contains " << needle << '\n';
-} */
+}
 
 #endif
--- CPP08/ex00/main.cpp
+++ CPP08/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: mpeshko <mpeshko@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/21 19:24:45 by mpeshko           #+#    #+#             */
-/*   Updated: 2025/08/22 21:00:58 by mpeshko          ###   ########.fr       */
+/*   Updated: 2025/08/23 15:59:15 by mpeshko          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,39 +16,85 @@
 #include <list>
 #include <deque>
 
-int main () 
+int main ()
 {
-    std::vector<unsigned>   u_vector;
-    unsigned                value;
-    
-    while (std::cin >> value) {
-        u_vector.push_back(value);
-    }
-    std::cout << "u_vector.size = " << u_vector.size() << std::endl;
+	// TEST 1: VECTOR
+	std::cout << "TEST 1: VECTOR" << std::endl;
 	
-	::easyfind(u_vector, 100);
-	::easyfind(u_vector, 98);
+	std::vector<int>	vector;
+	vector.push_back(45);
+	vector.push_back(65);
+	vector.push_back(99);
+	vector.push_back(100);
+	vector.push_back(34);
+	vector.push_back(-67);
+	vector.push_back(95);
+	vector.push_back(76);
+	vector.push_back(88);
+	vector.push_back(75);
+	vector.push_back(-83);
+	std::cout << "vector.size = " << vector.size() << std::endl;
 	
-	// 1. Check if element was found
-    std::vector<int>::iterator it = easyfind(u_vector, 30);
-    if (it != u_vector.end()) {
-        std::cout << "Found value: " << *it << std::endl;
-        std::cout << "At position: " << (it - u_vector.begin()) << std::endl;
-    } else {
-        std::cout << "Value not found!" << std::endl;
-    }
+	std::vector<int>::iterator it = easyfind(vector, 45);
+	if (it != vector.end()) {
+		std::cout << "Found value: " << *it << std::endl;
+		std::cout << "At position: " << (it - vector.begin()) << std::endl;
+	} else {
+		std::cout << "Value not found!" << std::endl;
+	}
 
+	it = easyfind(vector, 1);
+	if (it == vector.end())
+		std::cout << "Integer 1 isn't found" << std::endl;
 
-	//////
-
-
-	// 4. Use with different container types
-    std::list<int> lst;
-    lst.push_back(5);
-    lst.push_back(15);
-    lst.push_back(25);
-
+	// TEST 2: LIST
+	std::cout << "\nTEST 2: LIST" << std::endl;
 	
+	std::list<int>	lst;
+	lst.push_back(5);
+	lst.push_back(15);
+	lst.push_back(25);
+	std::cout << "list.size = " << lst.size() << std::endl;
+
+	// Note: std::list doesn't support random access, so no position calculation
+	// as we can do for vectors
+	// error: no operator "-" matches these operands
+	std::list<int>::iterator l_it = easyfind(lst, 25);
+	if (l_it != lst.end()) {
+		std::cout << "Found value: " << *l_it << std::endl;
+		// Calculate position manually for list
+		int	position = 0;
+		for (std::list<int>::iterator temp = lst.begin(); temp != l_it; ++temp) {
+			++position;
+		}
+		std::cout << "At position: " << position << std::endl;
+	} else {
+		std::cout << "Value not found!" << std::endl;
+	}
+
+	// TEST 3: DEQUE
+	std::cout << "\nTEST 3: DEQUE" << std::endl;
+	std::deque<int>	deque;
+	deque.push_back(5);
+	deque.push_back(15);
+	deque.push_back(25);
+	deque.push_back(-25);
+	std::cout << "deque.size = " << deque.size() << std::endl;
+
+	std::deque<int>::iterator d_it = easyfind(deque, 15);
+	if (d_it != deque.end()) {
+		std::cout << "Found value: " << *d_it << std::endl;
+		std::cout << "At position: " << (d_it - deque.begin()) << std::endl;
+	} else {
+		std::cout << "Value not found!" << std::endl;
+	}
+
+	// TEST 4: EMPTY CONTAINER
+	std::cout << "\nTEST 4: EMPTY CONTAINER" << std::endl;
+	std::vector<int> empty_vec;
+	std::vector<int>::iterator empty_it = easyfind(empty_vec, 42);
+	if (empty_it == empty_vec.end())
+		std::cout << "Empty container test passed" << std::endl;
 
 	return 0;
 }
